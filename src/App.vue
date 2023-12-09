da6b2536555bd2490fb75a4f4de502d3cadc1535
jingjingshenye <1976442140@qq.com>
2023-12-09 09:12:04 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+</script>
+
+<template>
+  <div class="container" ref="container">hello</div>
+</template>
+
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 如果你正在部署到 https://<USERNAME>.github.io/<REPO>/（例如你的仓库地址为 https://github.com<USERNAME>/<REPO>），那么请将 base 设置为 '/<REPO>/'。
+  base: '/threejs-blog'
+})
